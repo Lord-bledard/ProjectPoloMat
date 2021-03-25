cf3311006347dd311a2f1386332686a81563f84e
Pol-Antoine Loiseau <pol-antoine.loiseau@epitech.eu>
2021-03-25 21:48:04 +0100

    [Add] : Client

--- Client/include/client.hpp
+++ Client/include/client.hpp
@@ -1,1 +1,22 @@
 //client
+#ifndef __CLIENT_HPP__
+# define __CLIENT_HPP__
+
+# include					<exception>
+# include					<iostream>
+# include					<vector>
+# include					<algorithm>
+
+class Client{
+public:
+	Client(void);
+	~Client();
+
+    void init(const char *, int, int);
+	void run(void);
+
+private:
+	void setClient(void);
+};
+
+#endif                 /* !__CLIENT_HPP__ */
