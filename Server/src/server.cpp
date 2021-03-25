6e8f8c50e55c468dd338e57239f605500c9900f6
NinjaKinshasa <matthieu.david@epita.fr>
2021-03-25 20:43:22 +0100

    Added CMakeLists.txt with boost + hello world

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -1,1 +1,11 @@
 //server
+
+// Server-side Synchronous Chatting Application
+// using C++ boost::asio
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "ok" << std::endl;
+}
