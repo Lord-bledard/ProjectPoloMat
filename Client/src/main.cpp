3900786b0f3802d966b236bbf9a07ffd6b10377e
Pol-Antoine Loiseau <pol-antoine.loiseau@epitech.eu>
2021-03-25 21:28:15 +0100

    Add main type

---
+++ Client/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+
+int	main(int ac, char **av) {  
+try {
+  // Demarre Client
+    // Run le tout
+} catch (const std::exception &msg) {
+		  //gestion erreur
+	}
+}
